8fa1710fa418291ba0abd67f99277b8e7a015ff9
BullFei <tian18840876830@gmail.com>
2020-11-09 22:47:49 +0800

    跨域处理

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+// 项目的配置文件
+module.exports = {
+  devServer: {
+    proxy: {
+      // key: value
+      //    key - 前缀
+      //    value - 是跟 http-proxy-middleware 相同的一个配置项
+      "/migu": {
+        target: " https://query.asilu.com",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/migu": ""
+        }
+      }
+    }
+  }
+};
